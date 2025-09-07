df967941215fba79ad1ed4d46035c751fafd28f9
yogesh4492 <you@example.com>
2025-09-07 09:13:52 +0530

    new file:   demo.py 	new file:   python_assessment_348_1755350332.pdf 	new file:   repair_booking.py

---
+++ repair_booking.py
@@ -0,0 +1,27 @@
+def repairing_order_booking():
+    print(welcome.upper().center(55,"-"))
+    customer_name=input("Enter Customer Name:- ")
+    device_type=input("Enter Device Type:- ")
+    issue=input("Enter What is Issue:- ")
+    due_date=input("Enter Due Date (dd-mm-yyyy) :-")
+    return [customer_name,device_type,issue,due_date]
+
+welcome="welcome to the repair order booking system"
+print(welcome.upper().center(55,"-"))
+repairing_order_booking()
+# customer_name=input("Enter Customer Name:- ")
+# device_type=input("Enter Device Type:- ")
+# issue=input("Enter What is Issue:- ")
+# due_date=input("Enter Due Date (dd-mm-yyyy) :-")
+print("-"*55)
+choice=input("Do You Want To Generate Invoice  Press y :- ").lower()
+if choice=='y' or choice=='yes':
+    print("invoice".upper().center(55,"-"))
+    print(f"Customer Name :- {customer_name.upper()}")
+    print(f"Device Type   :- {device_type.upper()} ")
+    print(f"Issues        :- {issue.upper()} ")
+    print(f"Due Date      :- {due_date}")
+
+else:
+    print("thank you".upper().center(55,"-"))
+
