bac422ab2b4caa58a3d4e8adc165610212bbdc98
yogesh4492 <yogeshpatel26042005@gmail.com>
2025-10-06 11:15:45 +0530

    modified:   repair_booking.py

--- repair_booking.py
+++ repair_booking.py
@@ -4,11 +4,11 @@ def repairing_order_booking():
     device_type=input("Enter Device Type:- ")
     issue=input("Enter What is Issue:- ")
     due_date=input("Enter Due Date (dd-mm-yyyy) :-")
-    return [customer_name,device_type,issue,due_date]
+    return {'customer_name':customer_name,'device_type':device_type,'issue':issue,'due_date':due_date}
 
 welcome="welcome to the repair order booking system"
-print(welcome.upper().center(55,"-"))
-repairing_order_booking()
+# print(welcome.upper().center(55,"-"))
+dicts=repairing_order_booking()
 # customer_name=input("Enter Customer Name:- ")
 # device_type=input("Enter Device Type:- ")
 # issue=input("Enter What is Issue:- ")
@@ -17,10 +17,11 @@ print("-"*55)
 choice=input("Do You Want To Generate Invoice  Press y :- ").lower()
 if choice=='y' or choice=='yes':
     print("invoice".upper().center(55,"-"))
-    print(f"Customer Name :- {customer_name.upper()}")
-    print(f"Device Type   :- {device_type.upper()} ")
-    print(f"Issues        :- {issue.upper()} ")
-    print(f"Due Date      :- {due_date}")
+    print(f"Customer Name :- {dicts['customer_name'].upper()}")
+    print(f"Device Type   :- {dicts['device_type'].upper()} ")
+    print(f"Issues        :- {dicts['issue'].upper()} ")
+    print(f"Due Date      :- {dicts['due_date']}")
+    print("thank you".upper().center(55,"-"))
 
 else:
     print("thank you".upper().center(55,"-"))
